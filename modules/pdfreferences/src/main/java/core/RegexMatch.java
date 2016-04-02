efee5a39cf2d7be5c3b8366641932662b788d6c7
fred dupont <frederic.dupont44@yahoo.fr>
2016-04-02 23:57:57 +0200

    selection of folder

---
+++ modules/pdfreferences/src/main/java/core/RegexMatch.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core;
+
+/**
+ *
+ * @author fred
+ */
+public class RegexMatch {
+    public String mMatch;
+    public String mParsedName;
+    public String mRegexName;
+    
+    
+    
+}
